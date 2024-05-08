e6810f1b62917671821890d9260e5884a82d474f
Fouche du Preez <fouche@limehouse.co.za>
2024-05-08 21:43:37 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Fun from "./lib/Fun.svelte"
+
+    let color = "Green"
+</script>
+<h1>Hello World</h1>
+<Fun someColor={color}/>
---
+++ src/lib/Fun.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let someColor = 'green'
+    const changeColor = () => {
+        if (someColor === 'blue')
+            someColor = 'yellow'
+        else {
+            someColor = 'blue'
+        }
+    }
+</script>
+<h2 style="color: {someColor}">Svelte rocks!</h2>
+<button on:click={()=>changeColor()}>Change Color</button>
